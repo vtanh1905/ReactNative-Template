9dc1226e1fc2b72fe3a5f9b454b68feebfccdd96
Tuấn Anh <vtanh1905@gmail.com>
2020-04-15 11:25:15 +0700

    add folder

---
+++ src/components/ButtonRed.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Button } from 'react-native';
+
+function ButtonRed() {
+    return (
+        <View>
+            <Button />
+        </View>
+    )
+}
+
+export default ButtonRed
